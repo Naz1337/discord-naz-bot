4a2647b1d6c948be2a0f3c7105bde43d61675398
Naz1337 <mnazameeruln@gmail.com>
2021-07-23 04:22:42 +0800

    formatted the code little bit

--- naz_bot.py
+++ naz_bot.py
@@ -5,10 +5,12 @@ from discord import Intents
 from discord.ext import commands
 
 # BOT DECLARATION
-
-bot = commands.Bot(command_prefix="$", description="Bot that Naz made.", intents=Intents.all())
+bot = commands.Bot(command_prefix="$",
+                   description="Bot that Naz made.", intents=Intents.all())
 
 # BOT EVENTS
+
+
 @bot.event
 async def on_ready():
     print(
@@ -16,17 +18,22 @@ async def on_ready():
         f"Bot username: {bot.user}"
     )
 
+
 # BOT COMMANDS
 # COMMAND ERROR HANDLER MUST BE CODED BELOW THAT COMMAND
+
+
 @bot.command(name="ping", pass_context=True)
 async def ping(ctx: commands.Context):
     await ctx.send(f"Pong! {int(bot.latency*1000)}ms")
+
 
 @bot.command(aliases=["roll"])
 async def random_number(ctx: commands.Context, max: int):
     await ctx.send(
         f"Random generated number for you is {random.randint(0, max + 1)}"
     )
+
 
 @random_number.error
 async def random_number_error(ctx: commands.Context, error):
@@ -37,6 +44,7 @@ async def random_number_error(ctx: commands.Context, error):
         "Usage: $roll [number]"
     )
 
+
 @bot.command(aliases=["rld"])
 @commands.is_owner()
 async def reload(ctx: commands.Context, extension_name: str):
@@ -44,17 +52,20 @@ async def reload(ctx: commands.Context, extension_name: str):
 
     await ctx.send(f"Successfully reloaded {extension_name}")
 
+
 @reload.error
 async def reload_error(ctx: commands.Context, error):
     if isinstance(error, commands.NotOwner):
         await ctx.send("You do not have enough power to access this command.")
 
+
 # UTILITY FUNCTION
 
-# CODE TO RUN BEFORE STARTING BOT
 
+# CODE TO RUN BEFORE STARTING BOT
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
+        print(f"Loading {filename}")
         bot.load_extension(f"cogs.{filename[:-3]}")
 
 bot.run(os.getenv("DISCORDBOTAPIKEY"))
